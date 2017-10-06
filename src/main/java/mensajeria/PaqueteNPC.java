4112bb87e76f32772f807088ac21f6793f146aa2
Julián Ariel Martinez <julymartinez94@gmail.com>
2017-10-05 23:32:51 -0300

    Agrego npc al server

--- src/main/java/mensajeria/PaqueteNPC.java
+++ src/main/java/mensajeria/PaqueteNPC.java
@@ -7,127 +7,121 @@ import java.util.Iterator;
 
 import javax.swing.JOptionPane;
 
-import dominio.Item;
 import estados.Estado;
 
 public class PaqueteNPC extends Paquete implements Serializable, Cloneable {
-	//https://cdn-images-1.medium.com/max/455/1*snTXFElFuQLSFDnvZKJ6IA.png
-	
-	private int id;
-	private int idMapa;
-	private int estado;
-	private String nombre;
-	private int saludTope;
-	private int fuerza;
-	private int nivel = 1;
-	
-	public PaqueteNPC() throws IOException {
-		estado = Estado.estadoOffline;
-	}
+ //https://cdn-images-1.medium.com/max/455/1*snTXFElFuQLSFDnvZKJ6IA.png
+ 
+ private int id;
+ private int idMapa;
+ private int estado;
+ private String nombre;
+ private String raza = "NPC";
+ private int saludTope;
+ private int fuerza;
+ private int nivel = 1;
+ private float posX;
+ private float posY;
+ private int direccion;
+ 
+ public PaqueteNPC() throws IOException {
+  estado = Estado.estadoOffline;
+ }
 
-	public int getEstado() {
-		return estado;
-	}
+ public int getEstado() {
+  return estado;
+ }
 
-	public void setEstado(int estado) {
-		this.estado = estado;
-	}
+ public void setEstado(int estado) {
+  this.estado = estado;
+ }
 
-	public int getMapa(){
-		return idMapa;
-	}
+ public String getNombre() {
+  return nombre;
+ }
 
-	public void setMapa(int mapa){
-		idMapa = mapa;
-	}
+ public void setNombre(String nombre) {
+  this.nombre = nombre;
+ }
 
-	public int getNivel() {
-		return nivel;
-	}
+ public int getMapa(){
+  return idMapa;
+ }
+ public String getRaza() {
+  return raza;
+ }
 
-	public void setNivel(int nivel) {
-		this.nivel = nivel;
-	}
+ public void setMapa(int mapa){
+  idMapa = mapa;
+ }
 
-	public int getExperiencia() {
-		return 0;
-	}
+ public int getNivel() {
+  return nivel;
+ }
 
-	public void setExperiencia(int experiencia) {
-	}
+ public void setNivel(int nivel) {
+  this.nivel = nivel;
+ }
 
-	public int getId() {
-		return id;
-	}
+ public int getExperiencia() {
+  return 0;
+ }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+ public void setExperiencia(int experiencia) {
+ }
 
-	public String getCasta() {
-		return "";
-	}
+ public int getId() {
+  return id;
+ }
 
+ public void setId(int id) {
+  this.id = id;
+ }
 
-	public void setCasta(String casta) {		
-	}
+ public int getSaludTope() {
+  return saludTope;
+ }
 
-	public String getNombre() {
-		return nombre;
-	}
+ public void setSaludTope(int saludTope) {
+  this.saludTope = saludTope;
+ }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+ public int getFuerza() {
+  return fuerza;
+ }
 
-	public String getRaza() {
-		return "";
-	}
+ public void setFuerza(int fuerza) {
+  this.fuerza = fuerza;
+ }
 
-	public void setRaza(String raza) {
-	}
+ public float getPosX() {
+  return posX;
+ }
 
-	public int getSaludTope() {
-		return saludTope;
-	}
+ public void setPosX(float posX) {
+  this.posX = posX;
+ }
 
-	public void setSaludTope(int saludTope) {
-		this.saludTope = saludTope;
-	}
+ public float getPosY() {
+  return posY;
+ }
 
-	public int getEnergiaTope() {
-		return 0;
-	}
+ public void setPosY(float posY) {
+  this.posY = posY;
+ }
 
-	public void setEnergiaTope(int energiaTope) {
-	}
+ public int getDireccion() {
+  return direccion;
+ }
 
-	public int getFuerza() {
-		return fuerza;
-	}
+ public void setDireccion(int direccion) {
+  this.direccion = direccion;
+ }
 
-	public void setFuerza(int fuerza) {
-		this.fuerza = fuerza;
-	}
-
-	public int getDestreza() {
-		return 0;
-	}
-
-	public void setDestreza(int destreza) {
-	}
-
-	public int getInteligencia() {
-		return 0;
-	}
-
-	public void setInteligencia(int inteligencia) {
-	}
-
-	@Override
-	public Object clone() {
-		Object obj = null;
-		obj = super.clone();
-		return obj;
-	}	
+ @Override
+ public Object clone() {
+  Object obj = null;
+  obj = super.clone();
+  return obj;
+ } 
 }
