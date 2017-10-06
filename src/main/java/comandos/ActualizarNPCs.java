a1d93d676dd283c674e0b74e71ba27f6227dead9
Julián Ariel Martinez <julymartinez94@gmail.com>
2017-10-05 23:48:26 -0300

    Agrego para sync de npcs con server

---
+++ src/main/java/comandos/ActualizarNPCs.java
@@ -0,0 +1,13 @@
+package comandos; 
+ 
+import mensajeria.PaqueteDeNPCs; 
+ 
+public class ActualizarNPCs extends ComandosEscucha { 
+ 
+  @Override 
+  public void ejecutar() { 
+    PaqueteDeNPCs pdm = (PaqueteDeNPCs) gson.fromJson(cadenaLeida,PaqueteDeNPCs.class); 
+    juego.setNPCsDisponibles(pdm.getNPCs()); 
+  } 
+ 
+} 
