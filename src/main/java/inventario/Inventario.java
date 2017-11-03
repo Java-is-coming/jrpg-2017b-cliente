8ad9a7ef222a5c5a1a7415716444c8fb43b26e87
RodrigoCalvo <rodrigo.calvo@infiniteloop.com.ar>
2017-11-03 12:52:14 -0300

    Fix por merge

--- src/main/java/inventario/Inventario.java
+++ src/main/java/inventario/Inventario.java
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-package inventario;
-
-import java.awt.Color;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.swing.JPanel;
-import javax.swing.border.Border;
-import javax.swing.border.MatteBorder;
-
-import dominio.Item;
-import mensajeria.PaquetePersonaje;
-
-public class Inventario extends JPanel {
-    private static final int CANTCOLUMNAS = 3;
-    private static final int CANTFILAS = 3;
-    private final ArrayList<Item> items;
-
-    public Inventario(PaquetePersonaje paquetePersonaje) throws IOException {
-        setLayout(new GridBagLayout());
-        items = new ArrayList<Item>(paquetePersonaje.getItems());
-        final GridBagConstraints gbc = new GridBagConstraints();
-        for (int row = 0; row < CANTFILAS; row++) {
-            for (int col = 0; col < CANTCOLUMNAS; col++) {
-                gbc.gridx = col;
-                gbc.gridy = row; 
-                Celda cellPane;
-                if (!items.isEmpty()) {
-                    cellPane = new Celda(items.get(0), paquetePersonaje);
-                    items.remove(0);
-                } else {
-                    cellPane = new Celda();
-                }
-
-                Border border = null;
-                if (row < CANTFILAS - 1) {
-                    if (col < CANTCOLUMNAS - 1) {
-                        border = new MatteBorder(1, 1, 0, 0, Color.DARK_GRAY);
-                    } else {
-                        border = new MatteBorder(1, 1, 0, 1, Color.DARK_GRAY);
-                    }
-                } else {
-                    if (col < CANTCOLUMNAS - 1) {
-                        border = new MatteBorder(1, 1, 1, 0, Color.DARK_GRAY);
-                    } else {
-                        border = new MatteBorder(1, 1, 1, 1, Color.DARK_GRAY);
-                    }
-                }
-                cellPane.setBorder(border);
-                gbc.weightx = gbc.weighty = 1.0;
-                gbc.fill = GridBagConstraints.BOTH;
-
-                add(cellPane, gbc);
-            }
-        }
-    }
-}
-=======
 package inventario;
 
 import java.awt.Color;
@@ -80,57 +19,56 @@ import mensajeria.PaquetePersonaje;
  */
 public class Inventario extends JPanel {
 
-    private static final long serialVersionUID = 1L;
-    private static final int CANTCOLUMNAS = 3;
-    private static final int CANTFILAS = 3;
-    private final ArrayList<Item> items;
+	private static final long serialVersionUID = 1L;
+	private static final int CANTCOLUMNAS = 3;
+	private static final int CANTFILAS = 3;
+	private final ArrayList<Item> items;
 
-    /**
-     * Constructor de inventario
-     *
-     * @param paquetePersonaje
-     *            paquete
-     * @throws IOException
-     *             excepcion
-     */
-    public Inventario(final PaquetePersonaje paquetePersonaje) throws IOException {
-        setLayout(new GridBagLayout());
-        items = new ArrayList<Item>(paquetePersonaje.getItems());
-        final GridBagConstraints gbc = new GridBagConstraints();
-        for (int row = 0; row < CANTFILAS; row++) {
-            for (int col = 0; col < CANTCOLUMNAS; col++) {
-                gbc.gridx = col;
-                gbc.gridy = row;
-                Celda cellPane;
-                if (!items.isEmpty()) {
-                    cellPane = new Celda(items.get(0), paquetePersonaje);
-                    items.remove(0);
-                } else {
-                    cellPane = new Celda();
-                }
+	/**
+	 * Constructor de inventario
+	 *
+	 * @param paquetePersonaje
+	 *            paquete
+	 * @throws IOException
+	 *             excepcion
+	 */
+	public Inventario(final PaquetePersonaje paquetePersonaje) throws IOException {
+		setLayout(new GridBagLayout());
+		items = new ArrayList<Item>(paquetePersonaje.getItems());
+		final GridBagConstraints gbc = new GridBagConstraints();
+		for (int row = 0; row < CANTFILAS; row++) {
+			for (int col = 0; col < CANTCOLUMNAS; col++) {
+				gbc.gridx = col;
+				gbc.gridy = row;
+				Celda cellPane;
+				if (!items.isEmpty()) {
+					cellPane = new Celda(items.get(0), paquetePersonaje);
+					items.remove(0);
+				} else {
+					cellPane = new Celda();
+				}
 
-                Border border = null;
-                if (row < CANTFILAS - 1) {
-                    if (col < CANTCOLUMNAS - 1) {
-                        border = new MatteBorder(1, 1, 0, 0, Color.DARK_GRAY);
-                    } else {
-                        border = new MatteBorder(1, 1, 0, 1, Color.DARK_GRAY);
-                    }
-                } else {
-                    if (col < CANTCOLUMNAS - 1) {
-                        border = new MatteBorder(1, 1, 1, 0, Color.DARK_GRAY);
-                    } else {
-                        border = new MatteBorder(1, 1, 1, 1, Color.DARK_GRAY);
-                    }
-                }
-                cellPane.setBorder(border);
-                gbc.weightx = 1.0;
-                gbc.weighty = 1.0;
-                gbc.fill = GridBagConstraints.BOTH;
+				Border border = null;
+				if (row < CANTFILAS - 1) {
+					if (col < CANTCOLUMNAS - 1) {
+						border = new MatteBorder(1, 1, 0, 0, Color.DARK_GRAY);
+					} else {
+						border = new MatteBorder(1, 1, 0, 1, Color.DARK_GRAY);
+					}
+				} else {
+					if (col < CANTCOLUMNAS - 1) {
+						border = new MatteBorder(1, 1, 1, 0, Color.DARK_GRAY);
+					} else {
+						border = new MatteBorder(1, 1, 1, 1, Color.DARK_GRAY);
+					}
+				}
+				cellPane.setBorder(border);
+				gbc.weightx = 1.0;
+				gbc.weighty = 1.0;
+				gbc.fill = GridBagConstraints.BOTH;
 
-                add(cellPane, gbc);
-            }
-        }
-    }
+				add(cellPane, gbc);
+			}
+		}
+	}
 }
->>>>>>> origin/master
--- src/main/java/mensajeria/PaqueteDeNPCs.java
+++ src/main/java/mensajeria/PaqueteDeNPCs.java
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-package mensajeria;
-
-import java.io.Serializable;
-import java.util.Map;
-
-public class PaqueteDeNPCs extends Paquete implements Serializable, Cloneable {
-
-	private Map<Integer, PaqueteNPC> npcs;
-
-	public PaqueteDeNPCs() {
-
-	}
-
-	public PaqueteDeNPCs(Map<Integer, PaqueteNPC> npcs) {
-		this.npcs = npcs;
-	}
-
-	public Map<Integer, PaqueteNPC> getNPCs() {
-		return npcs;
-	}
-
-	@Override
-	public Object clone() {
-		Object obj = null;
-		obj = super.clone();
-		return obj;
-	}
-
-}
-=======
 package mensajeria;
 
 import java.io.Serializable;
@@ -40,41 +9,39 @@ import java.util.Map;
  */
 public class PaqueteDeNPCs extends Paquete implements Serializable, Cloneable {
 
-    private static final long serialVersionUID = 1L;
-    private Map<Integer, PaqueteNPC> npcs;
+	private static final long serialVersionUID = 1L;
+	private Map<Integer, PaqueteNPC> npcs;
 
-    /**
-     * Constructor
-     */
-    public PaqueteDeNPCs() {
+	/**
+	 * Constructor
+	 */
+	public PaqueteDeNPCs() {
 
-    }
+	}
 
-    /**
-     * Constructor con mapa de NPCs
-     *
-     * @param npcs
-     *            Mapa
-     */
-    public PaqueteDeNPCs(final Map<Integer, PaqueteNPC> npcs) {
-        this.npcs = npcs;
-    }
+	/**
+	 * Constructor con mapa de NPCs
+	 *
+	 * @param npcs
+	 *            Mapa
+	 */
+	public PaqueteDeNPCs(final Map<Integer, PaqueteNPC> npcs) {
+		this.npcs = npcs;
+	}
 
-    /**
-     * Getter del mapa de NPCs
-     *
-     * @return Map<Integer, PaqueteNPC> mapa
-     */
-    public Map<Integer, PaqueteNPC> getNPCs() {
-        return npcs;
-    }
+	/**
+	 * Getter del mapa de NPCs
+	 *
+	 * @return Map<Integer, PaqueteNPC> mapa
+	 */
+	public Map<Integer, PaqueteNPC> getNPCs() {
+		return npcs;
+	}
 
-    @Override
-    public Object clone() {
-        Object obj = null;
-        obj = super.clone();
-        return obj;
-    }
-
+	@Override
+	public Object clone() {
+		Object obj = null;
+		obj = super.clone();
+		return obj;
+	}
 }
->>>>>>> origin/master
